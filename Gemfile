c6f41eca464dd65d2639c0b7736c0de099459139
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2014-05-19 16:10:35 +0300

    most of the features are ready

--- Gemfile
+++ Gemfile
@@ -3,4 +3,6 @@ source 'http://rubygems.org'
 gem "middleman", "~>3.2.2"
 gem "middleman-livereload", "~> 3.1.0"
 gem "middleman-deploy"
+gem "middleman-blog"
+gem "redcarpet"
 gem "stringex"
--- config.rb
+++ config.rb
@@ -5,10 +5,29 @@ require "lib/custom_helpers"
 
 helpers CustomHelpers
 
+activate :i18n
+
+I18n.default_locale = :tr
+
+Time.zone = "Europe/Istanbul"
+
+activate :blog do |blog|
+  blog.paginate = true
+  blog.layout = "blog_layout"
+  blog.summary_separator = /READ_MORE/
+  blog.taglink = "etiket/{tag}.html"
+  blog.tag_template = "tag.html"
+end
+
+activate :directory_indexes
 activate :livereload
+
 set :css_dir, 'public/css'
 set :js_dir, 'public/js'
 set :images_dir, 'public/images'
+
+set :markdown_engine, :redcarpet
+set :markdown, :layout_engine => :erb, :autolink => true
 
 configure :build do
   activate :minify_css
